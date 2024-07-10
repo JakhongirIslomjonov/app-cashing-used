3029aea0f35369322876f206d520e713502c30c9
Jakhongir Islomjonov <jahongirnamti@gmail.com>
2024-07-10 21:11:59 +0500

    CacheManager used

--- src/main/java/com/example/appcachingused/AppCachingUsedApplication.java
+++ src/main/java/com/example/appcachingused/AppCachingUsedApplication.java
@@ -7,13 +7,18 @@ import lombok.SneakyThrows;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.net.CookieStore;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
+import java.util.List;
 
+@EnableScheduling
 @SpringBootApplication
 public class AppCachingUsedApplication {
 
@@ -30,4 +35,12 @@ public class AppCachingUsedApplication {
 
         };
     }
+
+    @Bean
+    public CacheManager cacheManager(){
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager();
+        cacheManager.setCacheNames(List.of("post"));
+        return cacheManager;
+    }
+
 }
--- src/main/java/com/example/appcachingused/service/PostService.java
+++ src/main/java/com/example/appcachingused/service/PostService.java
@@ -6,25 +6,36 @@ import com.example.appcachingused.mappers.PostMapper;
 import com.example.appcachingused.repo.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-@RequiredArgsConstructor
+
 @Service
 public class PostService {
 
 
     private final PostRepository postRepository;
     private final PostMapper postMapper;
+    private CacheManager cacheManager;
+    private final Cache cache;
 
-    private ConcurrentHashMap<Integer, PostDTO> cache = new ConcurrentHashMap<>();
+    public PostService(PostRepository postRepository, PostMapper postMapper, CacheManager cacheManager) {
+        this.postRepository = postRepository;
+        this.postMapper = postMapper;
+        this.cacheManager=cacheManager;
+        this.cache= cacheManager.getCache("post");
+    }
+//    private ConcurrentHashMap<Integer, PostDTO> cache = new ConcurrentHashMap<>();
 
     @SneakyThrows
     public PostDTO getOne(Integer id) {
-        PostDTO cachedPost = cache.get(id);
+        PostDTO cachedPost = cache.get(id,PostDTO.class);
         if (cachedPost != null) {
-            return  cachedPost;
+            return cachedPost;
         }
         Post post = postRepository.findById(id).orElseThrow(() -> new RuntimeException("error this id"));
         Thread.sleep(3000);
@@ -34,7 +45,7 @@ public class PostService {
 
     public void remove(Integer id) {
         postRepository.deleteById(id);
-        cache.remove(id);
+        cache.evict(id);
     }
 
     public void update(Integer id, PostDTO newPostDTO) {
@@ -44,6 +55,8 @@ public class PostService {
         post.setTitle(newPostDTO.getTitle());
         post.setBody(newPostDTO.getBody());
         postRepository.save(post);
-        cache.put(id,postMapper.toDto(post));
+        cache.put(id, postMapper.toDto(post));
     }
+
+
 }
